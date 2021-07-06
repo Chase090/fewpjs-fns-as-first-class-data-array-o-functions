9c8ffbb78ad8744319843faf18c053e013b06992
Chase090 <carloyam21@icloud.com>
2021-07-05 20:52:19 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,38 @@
+let dogName = `Jonas`
+let dogBreed = `Aussie Shepherd`
+
+function wakeDog(dogName, dogBreed){
+   const wd = `Wake ${dogName} the ${dogBreed}`;
+    return wd;
+}
+
+function leashDog(dogName, dogBreed) {
+    const ld =`Leash ${dogName} the ${dogBreed}`;
+    return ld;
+};
+
+function walkToPark(dogName, dogBreed) {
+    const wtp = `Walk to the park with ${dogName} the ${dogBreed}`;
+    return wtp;
+};
+
+function throwFrisbee(dogName, dogBreed) {
+    const tf = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    return tf;
+};
+
+function walkHome(dogName, dogBreed) {
+    const wh = `Walk home with ${dogName} the ${dogBreed}`;
+    return wh;
+};
+
+function unleashDog(dogName, dogBreed) {
+    const ud = `Unleash ${dogName} the ${dogBreed}`;
+    return ud; 
+};
+
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+   return routine.map(s => s(dogName, dogBreed))
+}
